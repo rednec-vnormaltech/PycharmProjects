98653dea15b749db3e1e41f0292120909165f2a7
normal07tech <normal07tech@gmail.com>
2022-01-05 02:02:42 +0300

    решено

---
+++ lesson_003/lecture_snippets/probe.py
@@ -0,0 +1,22 @@
+# Параметры передаются как ссылка
+
+def elephant_to_free(some_list):
+    elephant_found = 'elephant' in some_list
+    if elephant_found:
+        some_list.remove('elephant')
+        print('Слон на свободе!!!')
+    return elephant_found
+
+
+zoo = ['lion', 'elephant', 'monkey', 'skunk', 'horse', 'elephant']
+
+elephant_to_free(zoo)
+print(zoo)
+
+elephant_to_free(zoo)
+print(zoo)
+
+elephant_to_free(zoo)
+print(zoo)
+
+print(zoo)
