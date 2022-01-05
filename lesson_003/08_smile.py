ce986a40590f7a8cf4ad937dec877c656503e386
normal07tech <normal07tech@gmail.com>
2022-01-06 00:05:01 +0300

    решено

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
+
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO здесь ваш код
+def smile(x, y, color):
+    sd.circle(sd.get_point(x, y), radius=40, color=color, width=1)
+    sd.line(sd.get_point(x, y-10), sd.get_point(x, y+10), color=color, width=1)
+    sd.line(sd.get_point(x-15, y - 20), sd.get_point(x+15, y -20), color=color, width=1)
+    sd.line(sd.get_point(x - 15, y - 20), sd.get_point(x - 20, y - 10), color=color, width=1)
+    sd.line(sd.get_point(x + 15, y - 20), sd.get_point(x + 20, y - 10), color=color, width=1)
+    sd.circle(sd.get_point(x-17, y+10), radius=5, color=color, width=1)
+    sd.circle(sd.get_point(x + 17, y + 10), radius=5, color=color, width=1)
 
-simple_draw.pause()
+
+for _ in range(10):
+    smile(sd.random_point().x, sd.random_point().y, sd.COLOR_DARK_CYAN)
+sd.pause()
