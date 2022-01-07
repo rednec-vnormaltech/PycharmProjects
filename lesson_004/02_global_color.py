d63cd8f25b53a5404c93ba9b53b984607a125f3d
normal07tech <normal07tech@gmail.com>
2022-01-07 19:56:38 +0300

    решено

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import simple_draw as sd
+#sd.resolution = (1200, 800)
 
 # Добавить цвет в функции рисования геом. фигур. из упр lesson_004/01_shapes.py
-user_input = input("Введите число граней: ")
-face_count = int(user_input)
-print('Вы ввели', face_count)
-
 print('Возможные цвета:')
 menu_list = ['0 : red', '1 : orange', '2 : yellow', '3 : green', '4 : cyan', '5 : blue', '6 : purple', ]
 color_list = [sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE]
@@ -26,8 +23,17 @@ def count_angle(start_point, zero_angle, side_length, count_sides, count_colores
     sd.line(start_point, end_point, color_list[count_colores])
 
 
+point = sd.get_point(100, 100)
+count_angle(start_point=point, zero_angle=45, side_length=100, count_sides=3, count_colores=color_count)
+
 point = sd.get_point(400, 100)
-count_angle(start_point=point, zero_angle=45, side_length=200, count_sides=face_count, count_colores=color_count)
+count_angle(start_point=point, zero_angle=45, side_length=100, count_sides=4, count_colores=color_count)
+
+point = sd.get_point(100, 300)
+count_angle(start_point=point, zero_angle=45, side_length=100, count_sides=5, count_colores=color_count)
+
+point = sd.get_point(400, 300)
+count_angle(start_point=point, zero_angle=45, side_length=100, count_sides=6, count_colores=color_count)
 
 # Запросить у пользователя цвет фигуры посредством выбора из существующих:
 #   вывести список всех цветов с номерами и ждать ввода номера желаемого цвета.
