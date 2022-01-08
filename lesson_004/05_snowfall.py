c2cc8a5c2d319ec8231fd7ca72c0e809a42de24a
normal07tech <normal07tech@gmail.com>
2022-01-09 01:04:47 +0300

    три снежинки в бесконечном цикле

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
+import random as r
 import simple_draw as sd
 
+sd.resolution = (1500, 1200)
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
@@ -18,14 +19,49 @@ N = 20
 # sd.user_want_exit()
 
 # TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+
+list_point = [sd.get_point(100, 800), sd.get_point(300, 800), sd.get_point(500, 800), ]
+list_snowflake_parameters = [r.uniform(0.1, 0.9), r.uniform(0.1, 0.9), r.randint(2, 150), ]
+
+
+def snowfall():
+    length_snowflake = 50
+    # x, y = 100, 1200
+
+    while True:
+        sd.clear_screen()
+        sd.snowflake(center=list_point[0], length=length_snowflake, factor_a=list_snowflake_parameters[0],
+                     factor_b=list_snowflake_parameters[1], factor_c=list_snowflake_parameters[2])
+        list_point[0].y -= 6
+        if list_point[0].y < 50:
+            list_point[0].y = 1100
+
+        sd.snowflake(center=list_point[1], length=length_snowflake, factor_a=list_snowflake_parameters[0],
+                     factor_b=list_snowflake_parameters[1], factor_c=list_snowflake_parameters[2])
+        list_point[1].y -= 5
+        list_point[1].x += 2 / .5
+        if list_point[1].y < 50:
+            list_point[1].y = 900
+            list_point[1].x = 350
+
+        sd.snowflake(center=list_point[2], length=length_snowflake, factor_a=list_snowflake_parameters[0],
+                     factor_b=list_snowflake_parameters[1], factor_c=list_snowflake_parameters[2])
+        list_point[2].y -= 7
+        list_point[2].x += 2 / .9
+        if list_point[2].y < 50:
+            # break
+            list_point[2].y = 1000
+            list_point[2].x = 550
+            list_snowflake_parameters[0] = r.uniform(0.1, 0.9)
+            list_snowflake_parameters[1] = r.uniform(0.1, 0.9)
+            list_snowflake_parameters[2] = r.randint(2, 150)
+
+        sd.sleep(0.01)
+        if sd.user_want_exit():
+            break
+
+
+snowfall()
 
 sd.pause()
 
@@ -43,5 +79,3 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
-
-
