553daa4f58f583bbccd262468f4f2a0631512ce3
normal07tech <normal07tech@gmail.com>
2022-01-09 17:44:57 +0300

    задача решена

---
+++ lesson_005/01_inhabitants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from pprint import pprint
+
+import room_1 as rm1
+import room_2 as rm2
+
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+print('В комнате room_1 живут:',rm1.folks[0],' и ',rm1.folks[1], '\nВ комнате room_2 живет:',rm2.folks[0])
+
+# TODO здесь ваш код
+
+
