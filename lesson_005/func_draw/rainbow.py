44bf796ed3fb480de5cf709036c8dfc07c51d905
normal07tech <normal07tech@gmail.com>
2022-01-10 23:08:01 +0300

    1

--- lesson_005/func_draw/rainbow.py
+++ lesson_005/func_draw/rainbow.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+
 sd.resolution = (1200, 600)
+
 
 def draw_rainbow(centr, radius_0, width_0):
     rainbow_colors = [
-                        sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
-                        sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE
-                     ]
+        sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+        sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE
+    ]
 
-    for _ in range(10):
-        radius_start = radius_0
-        for x in rainbow_colors:
-            sd.circle(center_position=centr, radius=radius_start, color=x, width=width_0)
-            radius_start += 10
+    for x in rainbow_colors:
+        sd.circle(center_position=centr, radius=radius_0, color=x, width=width_0)
+        radius_0 += 10
 
 
-rb_point_centr = sd.get_point(300, -450)
-rb_start_radius = 1100
-rb_line_width = 10
-
-draw_rainbow(rb_point_centr, rb_start_radius, rb_line_width)
+# rb_point_centr = sd.get_point(300, -450)
+# rb_start_radius = 1100
+# rb_line_width = 10
+#
+# draw_rainbow(rb_point_centr, rb_start_radius, rb_line_width)
+# sd.pause()
