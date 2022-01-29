a7e5562b3132685b4f75b0e677e7b20180064cbb
normal07tech <normal07tech@gmail.com>
2022-01-29 04:41:04 +0300

    ничего не менялсь

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -1,62 +1,66 @@
 # -*- coding: utf-8 -*-
 
+import random as r
 import simple_draw as sd
 
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
 #  - отрисовку
+sd.resolution = (600, 600)
 
 
 class Snowflake:
 
-    def __init__(self, centr, length, color, factor_a, factor_b, factor_c):
-        self.centr = centr
-        self.length = length
-        self.color = color
-        self.factor_a = factor_a
-        self.factor_b = factor_b
-        self.factor_c = factor_c
+    def __init__(self, ):
+        self.point = sd.get_point(r.randint(100, 300), r.randint(450, 600))
+        self.length = r.randint(5, 30)
+        self.color = sd.COLOR_DARK_YELLOW
+        self.factor_a = 0.6
+        self.factor_b = 0.35
+        self.factor_c = r.randint(5, 90)
+
+    def create_snowfally(self):
+        sd.snowflake(self.point, self.length, self.color, self.factor_a, self.factor_b, self.factor_c)
 
     def clear_previous_picture(self):
         sd.clear_screen()
 
     def move(self):
-        pass
+        flake.clear_previous_picture()
+        flake.draw(self.point)
+        self.point.x += 2
+        self.point.y -= 10
+        if self.point.x > 600:
+            self.point.x = 100
+        if self.point.y < 10:
+            self.point.y = r.randint(450, 600)
+            flake.factor_c = r.randrange(5, 45, 5)
+            flake.length = r.randrange(5, 45, 5)
 
-    def draw(self, centr):
-        sd.snowflake(centr, self.length, self.color, self.factor_a, self.factor_b, self.factor_c)
+    def draw(self, point):
+        sd.snowflake(point, self.length, self.color, self.factor_a, self.factor_b, self.factor_c)
 
 
-x, y = 100, 600
-x1, y1 = 200, 550
 
-flake = Snowflake(sd.get_point(x, y), 30, sd.COLOR_DARK_YELLOW, 0.6, 0.35, 15)
-flake1 = Snowflake(sd.get_point(x1, y1), 30, sd.COLOR_DARK_YELLOW, 0.6, 0.35, 15)
+
+
+
 
 while True:
-    flake.clear_previous_picture()
+    flake = Snowflake()
+
+    # flake.clear_previous_picture()
+
+
     flake.move()
-    flake.draw(sd.get_point(x, y))
-    x += 2
-    y -= 10
-    if x > 600:
-        x = 100
-    if y < 10:
-        y = 10
-        x -= 2
-
-    flake1.draw(sd.get_point(x1, y1))
-    x1 += 2
-    y1 -= 10
-    if x1 > 600:
-        x1 = 100
-    if y1 < 10:
-        y1 = 600
 
 
-    sd.sleep(0.05)
 
+
+
+
+    sd.sleep(0.5)
 
     # if not flake.can_fall():
     #     break
@@ -64,7 +68,6 @@ while True:
     # if sd.user_want_exit():
     #     break
 # sd.pause()
-
 
 
 # шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
