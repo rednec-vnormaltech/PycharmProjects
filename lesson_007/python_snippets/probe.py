1057b4b393bd7d5275eed9ee614f76b50c79f941
normal07tech <normal07tech@gmail.com>
2022-01-29 04:40:28 +0300

    вася живой

--- lesson_007/python_snippets/probe.py
+++ lesson_007/python_snippets/probe.py
@@ -1,28 +1,59 @@
-from random import choice
+from termcolor import cprint
+import random as r
 
 
-class SomeClass:
-    x = 85
+class Man:
 
-    def __init__(self):
-        self.x = 178
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 100
+        self.food = 100
+        self.money = 100
 
-    def method_one(self):
-        x = 23
-        print('method_one', self.x)
+    def __str__(self):
+        return 'Я - {}, сытость {}, Еда {}, Деньги {} '.format(self.name, self.fullness, self.food, self.money)
 
-    def method_two(self):
-        x = 34
-        def func_one():
-             # x = 56
-            print('func_one', SomeClass.x)
+    def eat(self):
+        if self.food >= 10:
+            cprint('{} поел '.format(self.name), color='green')
+            self.fullness += 30
+            self.food -= 10
+        else:
+            cprint('У {} нет еды '.format(self.name), color='red')
 
-        func_one()
-        print('method_two', x)
+    def work(self):
+        cprint('{} сходил на работу '.format(self.name), color='blue')
+        self.money += 50
+        self.fullness -= 20
+
+    def shopping(self):
+        cprint('{} сходил в магазин,  '.format(self.name), color='cyan')
+        self.money -= 50
+        self.food += 20
+
+    def act(self):
+
+        dice = r.randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.food < 10:
+            self.shopping()
+        elif self.money < 50:
+            self.work()
+        elif dice == 1 or dice == 6:
+            self.work()
+        elif dice == 2 or dice == 4:
+            self.eat()
+        elif dice == 3 or dice == 5:
+            self.shopping()
 
 
-x = 12
-obj = SomeClass()
-obj.method_one()
-obj.method_two()
-print('global', x)
+vasya = Man(name="Василий")
+
+for day in range(1, 500):
+    print('\n================== день {} =================='.format(day))
+    print(vasya)
+    vasya.act()
+    if vasya.fullness <= 0:
+        cprint('{} умер...'.format(vasya.name), color='red')
+        break
